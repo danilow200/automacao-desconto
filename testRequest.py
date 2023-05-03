896a913dd79d2d860308337e6e373d63a91b7334
Danilo Domingo <40957754+danilow200@users.noreply.github.com>
2023-05-03 09:36:40 -0300

    feat: faz solicitação de desconto automatico

--- testRequest.py
+++ testRequest.py
@@ -1,59 +1,34 @@
 import requests
-import json
 import pandas as pd
 
-# class Desconto:
-#     def __init__(self, insert, ticket, observacao, inicio, fim, categoria):
-#         self.insert = insert
-#         self.ticket = ticket
-#         self.observacao = observacao
-#         self.inicio = inicio
-#         self.fim = fim
-#         self.categoria = categoria
+# Cria classe com todas as informações necessarias para criar um desconto
+class Desconto:
+    def __init__(self, insert, ticket, observacao, inicio, fim, categoria):
+        self.insert = insert
+        self.ticket = ticket
+        self.observacao = observacao
+        self.inicio = inicio
+        self.fim = fim
+        self.categoria = categoria
 
-# descontos = []
-# dados = []
+descontos = []
 
-# nome_do_arquivo = '.\\planilhas\\abril.xlsx'
+# Abri a planilha solicitada
+nome_do_arquivo = '.\\planilhas\\' + input('Insira o nome da planilha\n') + '.xlsx'
 
-# planilha = pd.read_excel(nome_do_arquivo,sheet_name='Codigos com desconto automatico')
-# planilha = planilha.drop(columns=['Unnamed: 0'])
-# planilha = planilha.drop(index=range(0,1),axis=0)
+planilha = pd.read_excel(nome_do_arquivo,sheet_name='Codigos com desconto automatico')
+# Exclui linhas e colunas extras
+planilha = planilha.drop(columns=['Unnamed: 0'])
+planilha = planilha.drop(index=range(0,1),axis=0)
 
+# Adiciona os tickets com descontos para um array de descontos utilizando a classe Desconto para estrutura-lo correntamente
+for index,row in planilha[1:].iterrows():
+    descontos.append(Desconto("descontos", row["Unnamed: 1"], "Desconto Automatico", row["Unnamed: 5"] + ':00', row["Unnamed: 6"] + ':00', row["Unnamed: 4"]))
 
-# for index,row in planilha[1:].iterrows():
-#     descontos.append(Desconto("descontos", row["Unnamed: 1"], "Desconto Automatico", row["Unnamed: 5"], row["Unnamed: 6"], row["Unnamed: 4"]))
-
-# for i in descontos:
-#     dados.append({
-#         "insert": i.insert, 
-#         "ticket": i.ticket, 
-#         "observacao": i.observacao, 
-#         "inicio": i.inicio, 
-#         "fim": i.fim, 
-#         "categoria": i.categoria
-#     })
-
-# dados_json = json.dumps(dados, indent=4)
-# print(dados_json)
-
-#_____________________________________________________________________________________________________________________
-
-data = { 
-            "insert":"descontos", 
-            "ticket":3297906, 
-            "observacao":"teste", 
-            "inicio":"03-04-2023 12:12:00", 
-            "fim":"03-04-2023 14:26:00", 
-            "categoria":"Terceiros"
-        }
-
-# Fazer uma solicitação GET para obter o cookie da sessão
-response = requests.get('https://report.telebras.com.br/index.php')
-cookie = {'PHPSESSID': '578a030e15574ea6c89b3b77590e9353'}
-
-# Fazer outra solicitação GET com o cookie da sessão
-url = 'https://report.telebras.com.br/pages/tickets/control.php'
-response = requests.post(url, cookies=cookie, data=data)
-
-print(response.status_code)
+# Roda o array de descontos e faz a solicitação de desconto
+for i in descontos:
+    url = "https://report.telebras.com.br/pages/tickets/control.php?insert=" + i.insert + '&ticket=' + str(i.ticket) + '&observacao=' + i.observacao + '&inicio=' + i.inicio + '&fim=' + i.fim + '&categoria=' + i.categoria
+    payload={} 
+    headers = { 'Cookie': 'PHPSESSID=578a030e15574ea6c89b3b77590e9353'} 
+    response = requests.request("POST", url, headers=headers, data=payload) 
+    print(response.text)
